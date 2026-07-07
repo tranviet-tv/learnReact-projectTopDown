a0b55d7619e19eba443f29d166f22d2cc5265da3
TRAN DUC VIET <tranducviet.tv@gmail.com>
2026-07-07 17:32:25 +0700

    refactor: Toi uu hoa goi API voi Promise.all, Axios va Async/Await

--- company-management/src/components/EmployeeList.jsx
+++ company-management/src/components/EmployeeList.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function EmployeeList() {
     const [employees, setEmployees] = useState([]);
@@ -6,18 +7,48 @@ export default function EmployeeList() {
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        fetch('http://localhost:8000/employees')
-            .then(response => response.json())
-            .then(data => setEmployees(data))
-            .catch(error => setError(error.message))
-            .finally(() => setIsLoading(false))
+    //cách 1: gọi đơn lẻ từng fetch API
+    //  useEffect(() => {
+    // fetch('http://localhost:8000/employees')
+    //     .then(response => response.json())
+    //     .then(data => setEmployees(data))
+    //     .catch(error => setError(error.message))
+    //     .finally(() => setIsLoading(false))
 
-        fetch('http://localhost:8000/departments')
-            .then(response => response.json())
-            .then(data => setDepartments(data))
-            .catch(error => setError(error.message))
-            .finally(() => setIsLoading(false))
+    // fetch('http://localhost:8000/departments')
+    //     .then(response => response.json())
+    //     .then(data => setDepartments(data))
+    //     .catch(error => setError(error.message))
+    //     .finally(() => setIsLoading(false))
+
+    //Cách 2: dùng promise để đồng bộ 2 fetch API 
+    //     useEffect(() => {
+    //     Promise.all([fetch('http://localhost:8000/employees').then(response => response.json()),
+    //     fetch('http://localhost:8000/departments').then(response => response.json())])
+    //         .then(([emp, dep]) => {
+    //             setEmployees(emp)
+    //             setDepartments(dep)
+    //         })
+    //         .catch(error => setError(error.message))
+    //         .finally(() => setIsLoading(false))
+    // }, []);
+
+    //Cách 3: dùng axios, async/await
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const [emp, dep] = await Promise.all([axios.get('http://localhost:8000/employees'), axios.get('http://localhost:8000/departments')])
+                setEmployees(emp.data)
+                setDepartments(dep.data)
+            }
+            catch (error) {
+                setError(error.message)
+            }
+            finally {
+                setIsLoading(false)
+            }
+        }
+        fetchData()
     }, []);
 
     if (isLoading) return <p className="text-info">Loading employees...</p>
@@ -27,6 +58,7 @@ export default function EmployeeList() {
         const department = departments.find(dep => dep.id == depId)
         return department ? department.depName : 'Not found'
     }
+
 
     return (
         <div>
